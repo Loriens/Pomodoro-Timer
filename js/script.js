6d04156dfbf66e7803ba97ca43dd9cdbd0c7f915
Loriens <lerd.95@mail.ru>
2016-02-27 16:19:44 +0300

    Buttons of the settings is ready

--- js/script.js
+++ js/script.js
@@ -1,32 +1,63 @@
 "use strict";
 
-function Timer(className) {
+function Timer() {
   var fullTime = 25 * 60 * 1000;
   var currentTime = fullTime;
-  var timerClass = document.getElementsByClassName(className)[0];
-  var buttonStartPause = document.getElementsByClassName("button")[0];
+  var timerClass;
+  var buttonStartPause;
+  var buttonsFullTime;
+  var buttonsBreakFullTime;
   var minutes;
   var seconds;
+  var breakFullTime = 5 * 60 * 1000;
   var timer;
   var self = this;
 
-  this.start = function() {
+  this.startPause = function() {
     if(!minutes && !seconds) self.setFullTime(fullTime);
     timer = setInterval(update, 1000);
     buttonStartPause.innerHTML = "Pause";
     buttonStartPause.onclick = self.stop;
   }
 
-  buttonStartPause.onclick = this.start;
-
   this.stop = function() {
     clearInterval(timer);
     buttonStartPause.innerHTML = "Start";
-    buttonStartPause.onclick = self.start;
   }
 
-  this.getButtonStartPause = function() {
-    return buttonStartPause;
+  this.setTimerClass = function(className) {
+    timerClass = document.getElementsByClassName(className)[0];
+  }
+
+  this.setButtonStartPause = function(nameButton) {
+    buttonStartPause = document.getElementsByClassName(nameButton)[0];
+    buttonStartPause.onclick = self.startPause;
+  }
+
+  this.setButtonsFullTime = function(nameButtons) {
+    buttonsFullTime = document.getElementsByName(nameButtons);
+    for(var i = 0; i < buttonsFullTime.length; i++) {
+      if(buttonsFullTime[i].value != getFullMinutes()) {
+        buttonsFullTime[i].checked = false;
+      } else {
+        buttonsFullTime[i].checked = true;
+      }
+
+      buttonsFullTime[i].onclick = changeFullTime;
+    }
+  }
+
+  this.setButtonsBreakFullTime = function(nameButtons) {
+    buttonsBreakFullTime = document.getElementsByName(nameButtons);
+    for(var i = 0; i < buttonsBreakFullTime.length; i++) {
+      if((buttonsBreakFullTime[i].value * 60 * 1000) != breakFullTime) {
+        buttonsBreakFullTime[i].checked = false;
+      } else {
+        buttonsBreakFullTime[i].checked = true;
+      }
+
+      buttonsBreakFullTime[i].onclick = changeBreakFullTime;
+    }
   }
 
   this.defaultFullTime = function() {
@@ -38,10 +69,32 @@ function Timer(className) {
 
   this.setFullTime = function(newFullTime) {
     if(newFullTime > 0 && newFullTime < 60 * 60 * 1000) fullTime = newFullTime;
-    seconds = (fullTime % (1000 * 60)) / 1000;
-    minutes = (fullTime - seconds * 1000) / (1000 * 60);
+    seconds = getFullSeconds();
+    minutes = getFullMinutes();
     setTimeHtml();
     minusSecond();
+  }
+
+  this.setBreakFullTime = function(newBreakFullTime) {
+    breakFullTime = newBreakFullTime;
+  }
+
+  var changeFullTime = function() {
+    for(var i = 0; i < buttonsFullTime.length; i++) {
+      if(buttonsFullTime[i].checked) {
+        self.setFullTime(buttonsFullTime[i].value * 60 * 1000);
+        return;
+      }
+    }
+  }
+
+  var changeBreakFullTime = function() {
+    for(var i = 0; i < buttonsBreakFullTime.length; i++) {
+      if(buttonsBreakFullTime[i].checked) {
+        self.setBreakFullTime(buttonsBreakFullTime[i].value * 60 * 1000);
+        return;
+      }
+    }
   }
 
   var update = function() {
@@ -63,5 +116,13 @@ function Timer(className) {
       seconds = 59;
       minutes--;
     }
+  }
+
+  var getFullSeconds = function() {
+    return (fullTime % (1000 * 60)) / 1000;
+  }
+
+  var getFullMinutes = function() {
+    return (fullTime - getFullSeconds() * 1000) / (1000 * 60);
   }
 }
